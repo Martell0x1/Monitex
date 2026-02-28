9fdd0ac4d43c7af3e0e0b175d1488f4ae6961fff
Martell0x1 <marawanzein222@gmail.com>
2026-02-28 07:19:33 +0200

    fixing working dir

---
+++ Front-Web/src/app/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Divider } from '../divider/divider';
+
+@Component({
+  selector: 'app-login',
+  imports: [Divider],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+
+}
